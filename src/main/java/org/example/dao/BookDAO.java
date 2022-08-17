56f5dd3a18e75555bf5d5c0b5acb9c54ac98317a
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-17 13:52:24 +0200

    Create BookController and BookDAO

---
+++ src/main/java/org/example/dao/BookDAO.java
@@ -0,0 +1,35 @@
+package org.example.dao;
+
+import org.example.models.Book;
+import org.example.models.Person;
+
+import java.util.Optional;
+
+/**
+ * @author Roman_Andriiv
+ */
+public class BookDAO {
+    public Object index() {
+    }
+
+    public Object show(int id) {
+    }
+
+    public Optional<Person> getBookOwner(int id) {
+    }
+
+    public void save(Book book) {
+    }
+
+    public void update(int id, Book book) {
+    }
+
+    public void delete(int id) {
+    }
+
+    public void release(int id) {
+    }
+
+    public void assign(int id, Person selectedPerson) {
+    }
+}
