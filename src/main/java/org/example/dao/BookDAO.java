9c676cdffbaf2ac5323a53f19f463d3fa55bc795
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-17 14:55:18 +0200

    Implement BookDAO methods

--- src/main/java/org/example/dao/BookDAO.java
+++ src/main/java/org/example/dao/BookDAO.java
@@ -2,34 +2,63 @@ package org.example.dao;
 
 import org.example.models.Book;
 import org.example.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
  * @author Roman_Andriiv
  */
+@Controller
 public class BookDAO {
-    public Object index() {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Object show(int id) {
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));
     }
 
-    public Optional<Person> getBookOwner(int id) {
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT * FROM book WHERE id =?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Book.class)).stream().findAny().orElse(null);
     }
 
     public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO book(title,author, year) VALUES(?, ?,?)", book.getTitle(),
+                book.getAuthor(), book.getYear());
     }
 
-    public void update(int id, Book book) {
+    public void update(int id, Book updatedBook) {
+        jdbcTemplate.update("UPDATE book SET title=?, author=?, year=? WHERE id=?", updatedBook.getTitle(),
+                updatedBook.getAuthor(), updatedBook.getYear(), id);
     }
 
     public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM book WHERE id = ?", id);
     }
 
+//Join Book and Person tables and get the person who owns the book with the specified id
+    public Optional<Person> getBookOwner(int id) {
+        return jdbcTemplate.query("SELECT person.* FROM book JOIN person ON book.person_id = person.id " +
+                "WHERE book.id = ?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
+    }
+
+//Releases the book (this method is called when a person returns a book to the library)
     public void release(int id) {
+        jdbcTemplate.update("UPDATE book SET person_id=NULL WHERE id =?", id);
     }
 
+//Assigns a book to a person (this method is called when a person checks out a book from the library)
     public void assign(int id, Person selectedPerson) {
+        jdbcTemplate.update("UPDATE book SET person_id=? WHERE id=?", selectedPerson.getId(), id);
     }
 }
