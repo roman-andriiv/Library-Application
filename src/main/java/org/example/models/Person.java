520bb39f62c66e9f12d756c405e92195119ec9aa
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-25 09:38:14 +0200

    add javax.persistence @Annotations to Person

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,19 +1,27 @@
 package org.example.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
 
 /**
  * @author Roman_Andriiv
  */
+@Entity
+@Table(name = "Person")
 public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
+    @Column(name = "full_name")
     private String fullName;
 
     @Min(value = 1900, message = "Age should be greater than 1900")
+    @Column(name = "year_of_birth")
     private int yearOfBirth;
 
     public Person() {
@@ -49,4 +57,11 @@ public class Person {
         this.yearOfBirth = yearOfBirth;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                '}';
+    }
 }
