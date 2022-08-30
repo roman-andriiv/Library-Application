2b565fe5f1f79cf9fdd0879a1c63a760f008e48c
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-30 17:51:28 +0200

    add method for finding books by title

--- src/main/java/org/example/repositories/BooksRepository.java
+++ src/main/java/org/example/repositories/BooksRepository.java
@@ -4,10 +4,13 @@ import org.example.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Roman_Andriiv
  */
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleStartingWith(String title);
 
 }
