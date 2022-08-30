dd42d02b22dc0915556fbee2e93e5b6126e6fa75
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-30 17:56:55 +0200

    add method for finding person by full name

--- src/main/java/org/example/util/PersonValidator.java
+++ src/main/java/org/example/util/PersonValidator.java
@@ -1,7 +1,7 @@
 package org.example.util;
 
-import org.example.services.PeopleService;
 import org.example.models.Person;
+import org.example.services.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -12,11 +12,11 @@ import org.springframework.validation.Validator;
  */
 @Component
 public class PersonValidator implements Validator {
-    private final PeopleService personDAO;
+    private final PeopleService peopleService;
 
     @Autowired
-    public PersonValidator(PeopleService personDAO) {
-        this.personDAO = personDAO;
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
     @Override
@@ -28,7 +28,7 @@ public class PersonValidator implements Validator {
     public void validate(Object o, Errors errors) {
         Person person = (Person) o;
 
-        if (personDAO.getPersonByFullName(person.getFullName()).isPresent()){
+        if (peopleService.getPersonByFullName(person.getFullName()).isPresent()){
             errors.rejectValue("fullName", "", "Person with this name is already exists");
         }
     }
