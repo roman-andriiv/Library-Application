178cdccc21a93e13ef9dc0284871274c06868985
Roman Andriiv <roman.andriyiv@gmail.com>
2022-08-31 10:24:58 +0200

    refactor BookController

--- src/main/java/org/example/controllers/BookController.java
+++ src/main/java/org/example/controllers/BookController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 /**
  * @author Roman_Andriiv
@@ -29,21 +28,25 @@ public class BookController {
     }
 
     @GetMapping
-    public String index(Model model) {
-
-        model.addAttribute("books", bookService.index());
+    public String index(Model model, @RequestParam(value = "page", required = false) Integer page,
+                        @RequestParam(value = "books_per_page", required = false) Integer booksPerPage,
+                        @RequestParam(value = "sort_by_year", required = false) boolean sortByYear) {
+        if (page == null || booksPerPage == null)
+            model.addAttribute("books", bookService.findAll(sortByYear));
+        else
+            model.addAttribute("books", bookService.findWithPagination(page, booksPerPage, sortByYear));
         return "books/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model, @ModelAttribute("person") Person person) {
 
-        model.addAttribute("book", bookService.show(id));
+        model.addAttribute("book", bookService.findOne(id));
 
-        Optional<Person> bookOwner = bookService.getBookOwner(id);
+        Person bookOwner = bookService.getBookOwner(id);
 
-        if (bookOwner.isPresent()) {
-            model.addAttribute("owner", bookOwner.get());
+        if (bookOwner != null) {
+            model.addAttribute("owner", bookOwner);
         } else {
             model.addAttribute("people", peopleService.findAll());
         }
@@ -66,7 +69,7 @@ public class BookController {
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("book", bookService.show(id));
+        model.addAttribute("book", bookService.findOne(id));
         return "book/edit";
     }
 
@@ -93,9 +96,20 @@ public class BookController {
     }
 
     @PatchMapping("/{id}/assign")
-    public String assign(@PathVariable("id") int id, @ModelAttribute("person") Person selectedPerson){
+    public String assign(@PathVariable("id") int id, @ModelAttribute("person") Person selectedPerson) {
         bookService.assign(id, selectedPerson);
         return "redirect:/books/" + id;
+    }
+
+    @GetMapping("/search")
+    public String searchPage() {
+        return "books/search";
+    }
+
+    @PostMapping("/search")
+    public String makeSearch(Model model, @RequestParam("query") String query) {
+        model.addAttribute("books", bookService.searchByTitle(query));
+        return "books/search";
     }
 
 }
